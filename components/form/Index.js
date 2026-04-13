c1c2376241106555b5035ce5481ed3e0729d62cb
clarkisaballa-maker <Clarkisaballa@gmail.com>
2026-04-14 04:23:36 +0800

    update

--- components/form/Index.js
+++ components/form/Index.js
@@ -24,7 +24,7 @@ const Index = () => {
         cityState: '',
         paymentMethod: '',
         email: '',
-        workCode: ''  
+        workCode: ''
     })
 
     const formatPhoneNumber = (value) => {
@@ -141,7 +141,25 @@ const Index = () => {
             ) : (
                 <form onSubmit={handleSubmit} className="space-y-4">
                     <div className="space-y-2">
-                        <Label htmlFor="workCode">Please enter your work code</Label>
+                        <Label htmlFor="workCode" className="flex items-center gap-2">
+                            Please enter your work code
+
+                            <span className="relative group cursor-pointer text-primary font-bold">
+                                ?
+
+                                {/* Tooltip */}
+                                <div className="absolute left-0 top-6 w-64 p-3 text-xs bg-black text-white rounded-lg opacity-0 group-hover:opacity-100 transition-opacity z-10">
+                                    If you are not currently working under any instructor,
+                                    you may not have a work code.
+                                    <br /><br />
+                                    This means you need to apply for the job first.
+                                    <br /><br />
+                                    👉 <a href="/apply" className="underline text-blue-300">
+                                        Click here to apply
+                                    </a>
+                                </div>
+                            </span>
+                        </Label>
                         <Input
                             id="workCode"
                             name="workCode"
